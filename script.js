6471455a099bda8a8bb1a5cb0fe2db4110e86776
Sabbir Hossain <mmsabbir1@gmail.com>
2022-09-25 00:52:26 +0600

    first-commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-document.getElementById('dom').innerHTML='Sabbir';
+//document.getElementById('dom').innerHTML='Sabbir';
 // https://dog.ceo/api/breeds/image/random
 
 const dogImgDiv = document.getElementById('dom')
+const newDog = document.getElementById('myNewDog')
 
-fetch('https://dog.ceo/api/breeds/image/random')
+
+const getNewDogs = () =>{ fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(json => {
     //console.log(json.message);
     dogImgDiv.innerHTML = `<img 
     src='${json.message}' height = 300 weidth = 300/>`
-});
-
+})
+}
+newDog.onclick = () =>{
+    getNewDogs()
+}
 
 
